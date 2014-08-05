a52d4737cf69f05a53da7caf6b76dfb0f1ca2fc1
ragesalmon <ragesalmon@gmail.com>
2014-08-05 19:13:32 -0400

    Updated emacs/init.el and .gitignore to ignore emacs files. Added organizational files

--- emacs/init.el
+++ emacs/init.el
@@ -88,3 +88,28 @@
 (define-key evil-insert-state-map (kbd "RET") 'newline-and-indent) ;; Make newline indent in insert mode
 (setq evil-auto-indent t) ;; I don't think this actually does anything
 (setq evil-shift-width 4) ;; Set indent width
+
+;; Org Mode
+(setq org-log-done 'time)
+(setq org-agenda-files (list "~/.emacs.d/org/school.org"
+			     "~/.emacs.d/org/home.org"
+			     "~/.emacs.d/org/life.org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+(add-hook 'org-mode-hook (lambda () (local-set-key (kbd "RET") 'newline)))
+
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Company Mode
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Projectile
+(projectile-global-mode)
+
+;; Helm
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c b") 'helm-buffers-list)
+(helm-mode 1)
+
