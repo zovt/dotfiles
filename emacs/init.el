b062a79b9c2a9de3c04060baad19917319c9a4c0
zovt <zovtly@gmail.com>
2015-07-17 12:03:18 -0400

    Fixed Web-mode markup offset

--- emacs/init.el
+++ emacs/init.el
@@ -291,6 +291,9 @@
 			   (setq web-mode-markup-indent-offset 2)
 			   (setq web-mode-css-indent-offset 2)
 			   (setq web-mode-code-indent-offset 2)))
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 ;;; Haskell Mode
 ;; Enable indentation
@@ -387,3 +390,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
