a57808a7191dbda8957358212affb1fcdeee5a73
zovt <zovtly@gmail.com>
2016-03-05 00:55:46 -0500

    Updates

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,8 +4,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" default))))
+	 (quote
+		("0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "98a619757483dc6614c266107ab6b19d315f93267e535ec89b7af3d62fb83cad" "9b402e9e8f62024b2e7f516465b63a4927028a7055392290600b776e4a5b9905" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" default)))
+ '(pos-tip-background-color "#36473A")
+ '(pos-tip-foreground-color "#FFFFC8")
+ '(safe-local-variable-values
+	 (quote
+		((eval setq-local flycheck-clang-include-path
+					 (list
+						(expand-file-name
+						 (concat
+							(locate-dominating-file default-directory ".dir-locals.el")
+							"include/"))))
+		 (eval setq-local flycheck-clang-include-path
+					 (list
+						(concat
+						 (locate-dominating-file default-directory ".dir-locals.el")
+						 "include/")))
+		 (flycheck-clang-include-path list
+																	(expand-file-name "include/"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,8 @@
 ;; visual
-(set-face-font 'default "Hack-12")
+(set-face-font 'default "Envy Code R-12")
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)
-(setq visual-bell 1)
+(setq visible-bell t)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
@@ -13,71 +13,106 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
+;; indentation settings
+(setq-default c-basic-offset 2
+							tab-width 2
+							indent-tabs-mode t)
+
 ;; set up packages
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("marmalade"
-				 . "http://marmalade-repo.org/packages/") t)
+																 . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (if (not (package-installed-p 'use-package))
-    (progn (package-refresh-contents)
-	   (package-install 'use-package)))
+		(progn (package-refresh-contents)
+					 (package-install 'use-package)))
 
 ;; turn on electric-pair-mode
 (electric-pair-mode)
 
 ;; begin packages
-;; ample theme
-(use-package ample-theme
-  :ensure t
-  :config
-  (load-theme 'ample-flat))
+;; noctilux theme
+(use-package noctilux-theme
+	:ensure t
+	:config
+	(load-theme 'noctilux))
 
 ;; evil
 (use-package evil
-  :ensure t
-  :config
-  (evil-mode)
-  (evil-global-set-key 'normal (kbd "<SPC>")
-		       (lambda ()
-			 (interactive)
-			 (setq unread-command-events
-			       (listify-key-sequence "\C-c"))))
-  (evil-global-set-key 'normal ";" 'evil-ex)
-  (evil-global-set-key 'normal "U" 'undo-tree-visualize))
+	:ensure t
+	:config
+	(evil-mode)
+	(evil-global-set-key 'normal (kbd "<SPC>")
+											 (lambda ()
+												 (interactive)
+												 (setq unread-command-events
+															 (listify-key-sequence "\C-c"))))
+	(evil-global-set-key 'normal ";" 'evil-ex)
+	(evil-global-set-key 'normal "U" 'undo-tree-visualize))
 
 ;; undo-tree
 (use-package undo-tree
-  :ensure t)
+	:ensure t)
 
 ;; smex
 (use-package smex
-  :ensure t
-  :config
-  (global-set-key (kbd "M-x") 'smex))
+	:ensure t
+	:config
+	(global-set-key (kbd "M-x") 'smex))
+
+;; irony-mode
+(use-package irony
+	:ensure t
+	:config
+	(add-hook 'c++-mode-hook 'irony-mode)
+	(if (string-equal system-type "windows-nt")
+			(setq w32-pipe-read-delay 0)))
+
+;; cpputils-cmake
+(use-package cpputils-cmake
+	:ensure t
+	:config
+	(add-hook 'c++-mode-hook 'cppcm-reload-all))
 
 ;; company
+(use-package company-irony
+	:ensure t)
+
+(use-package company-irony-c-headers
+	:ensure t)
+
+(use-package company-c-headers
+	:ensure t)
+
 (use-package company
-  :ensure t
-  :config
-  (setq company-backends '(company-clang company-elisp company-dabbrev company-dabbrev-code))
-  (global-company-mode))
+	:ensure t
+	:config
+	(setq company-backends '(company-irony company-irony-c-headers company-c-headers company-clang company-elisp company-dabbrev company-dabbrev-code))
+	(global-company-mode))
+
+;; smart-tabs
+(use-package smart-tabs-mode
+	:ensure t
+	:config
+	(smart-tabs-insinuate 'c++ 'c 'java 'javascript))
 
 ;; flycheck
 (use-package flycheck
-  :ensure t
-  :config
-  (global-flycheck-mode))
+	:ensure t
+	:config
+	(setq-default flycheck-clang-language-standard "c++14")
+	(global-flycheck-mode))
 
-;; rainbow-delimiters-mode
-(use-package rainbow-delimiters
-  :ensure t
-  :config
-  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode-enable)
-  (add-hook 'c++-mode-hook 'rainbow-delimiters-mode-enable))
+	;; rainbow-delimiters-mode
+	(use-package rainbow-delimiters
+		:ensure t
+		:config
+		(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode-enable)
+		(add-hook 'c++-mode-hook 'rainbow-delimiters-mode-enable))
 
 ;; org-mode
 (setq org-log-done 'time)
@@ -86,25 +121,33 @@
 
 ;; minimap
 (use-package minimap
-  :ensure t
-  :config
-  (setq minimap-window-location 'right))
+	:ensure t
+	:config
+	(setq minimap-window-location 'right))
 
 ;; smooth-scrolling
 (use-package smooth-scrolling
-  :ensure t
-  :config
-  (smooth-scrolling-mode 1))
+	:ensure t
+	:config
+	(smooth-scrolling-mode 1))
 
 ;; aggressive-indent-mode
 (use-package aggressive-indent
-  :ensure t
-  :config
-  (global-aggressive-indent-mode 1))
+	:ensure t
+	:config
+	(add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode))
 
 ;; fiplr
 (use-package fiplr
-  :ensure t)
+	:ensure t)
+
+;; diminish
+(use-package diminish
+	:ensure t
+	:config
+	(diminish 'aggressive-indent-mode)
+	(diminish 'company-mode)
+	(diminish 'undo-tree-mode))
 
 ;; keybinds
 ;; fix escape
@@ -113,13 +156,16 @@
 ;; window management
 (global-set-key (kbd "C-c w j") 'windmove-down)
 (global-set-key (kbd "C-c w k") 'windmove-up)
-(global-set-key (kbd "C-c w l") 'windmove-left)
-(global-set-key (kbd "C-c w h") 'windmove-right)
+(global-set-key (kbd "C-c w l") 'windmove-right)
+(global-set-key (kbd "C-c w h") 'windmove-left)
 (global-set-key (kbd "C-c w q") 'delete-window)
+(global-set-key (kbd "C-c w H") 'split-window-horizontally)
+(global-set-key (kbd "C-c w V") 'split-window-vertically)
 
 ;; buffers
 (global-set-key (kbd "C-c b n") 'next-buffer)
 (global-set-key (kbd "C-c b p") 'previous-buffer)
+(global-set-key (kbd "C-c b b") 'switch-to-buffer)
 
 ;; minimap
 (global-set-key (kbd "C-c t m") 'minimap-mode)
@@ -129,3 +175,15 @@
 
 ;; fiplr
 (global-set-key (kbd "C-c f") 'fiplr-find-file)
+
+;; customizing the modeline
+(setq-default mode-line-format
+							(list '(:eval (propertize "  %b" 'help-echo (buffer-file-name)))
+										'(:eval (if (buffer-modified-p)
+																(propertize "%*%* ")
+															(propertize "   ")))
+										'(:eval (propertize "(%l:%c)  "))
+										'(:eval (propertize "[%m]  "))
+										'(:eval (propertize "{"))
+										minor-mode-alist
+										'(:eval (propertize " } "))))
