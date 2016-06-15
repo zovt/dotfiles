d3d8d29f9dbac987297e749bec32d694803bb93f
zovt <zovtly@gmail.com>
2016-06-15 10:05:17 -0400

    added HTML to web-mode

--- emacs/init.el
+++ emacs/init.el
@@ -185,7 +185,8 @@
 (use-package web-mode
 	:ensure t
 	:config
-	(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode)))
+	(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+	(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode)))
 
 ;; keybinds
 
