1573413da468e4ed255ae5fa5233af9a88165a8a
zovt <zovt@posteo.de>
2017-06-02 16:16:48 -0400

    emacs: update telephone-line

--- emacs/init.el
+++ emacs/init.el
@@ -176,9 +176,9 @@
   :config
   (setq-default telephone-line-height 30
                 telephone-line-primary-left-separator 'telephone-line-cubed-left
-                telephone-line-secondary-left-separator 'telephone-line-cubed-left
+                telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
                 telephone-line-primary-right-separator 'telephone-line-cubed-right
-                telephone-line-secondary-right-separator 'telephone-line-cubed-right)
+                telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
   (telephone-line-mode t))
 
 ;; prog langs
