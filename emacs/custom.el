68d8ffa1d186dd8a5d41d60e1244c40e2030d146
zovt <zovt@posteo.de>
2018-01-08 21:21:15 -0500

    Emacs updates

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,20 +3,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+	 ["#E8E8E8" "#3C3C3C" "#616161" "#0E0E0E" "#252525" "#3C3C3C" "#171717" "#0E0E0E"])
  '(custom-safe-themes
-   (quote
-    ("ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "dd1cf47034b1c20f5f43cd91ae76f00abef05f91b7be57d94653c493bcf41dda" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "ad9747dc51ca23d1c1382fa9bd5d76e958a5bfe179784989a6a666fe801aadf2" default)))
+	 (quote
+		("0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "ba7917b02812fee8da4827fdf7867d3f6f282694f679b5d73f9965f45590843a" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "dd1cf47034b1c20f5f43cd91ae76f00abef05f91b7be57d94653c493bcf41dda" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "ad9747dc51ca23d1c1382fa9bd5d76e958a5bfe179784989a6a666fe801aadf2" default)))
  '(fci-rule-color "#f8fce8")
  '(hl-paren-background-colors (quote ("#e8fce8" "#c1e7f8" "#f8e8e8")))
  '(hl-paren-colors (quote ("#40883f" "#0287c8" "#b85c57")))
  '(magit-commit-arguments (quote ("--gpg-sign=62E16BF6B9B05F07")))
  '(package-selected-packages
-   (quote
-    (tao-theme kaolin-theme markdown-mode org-plus-contrib which-key use-package smooth-scrolling smart-tabs-mode ripgrep restclient rainbow-delimiters racer nlinum-relative neotree multiple-cursors mips-mode leuven-theme ledger-mode flycheck-rust exec-path-from-shell evil-magit diff-hl counsel company-go all-the-icons-dired ace-window)))
+	 (quote
+		(cmake-ide company-cmake cmake-mode tao-theme kaolin-theme markdown-mode org-plus-contrib which-key use-package smooth-scrolling smart-tabs-mode ripgrep restclient rainbow-delimiters racer nlinum-relative neotree multiple-cursors mips-mode leuven-theme ledger-mode flycheck-rust exec-path-from-shell evil-magit diff-hl counsel company-go all-the-icons-dired ace-window)))
  '(sml/active-background-color "#98ece8")
  '(sml/active-foreground-color "#424242")
  '(sml/inactive-background-color "#4fa8a8")
- '(sml/inactive-foreground-color "#424242"))
+ '(sml/inactive-foreground-color "#424242")
+ '(vc-annotate-background "#F6F6F6")
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#C3C3C3")
+		 (40 . "#9E9E9E")
+		 (60 . "#9E9E9E")
+		 (80 . "#616161")
+		 (100 . "#616161")
+		 (120 . "#3C3C3C")
+		 (140 . "#3C3C3C")
+		 (160 . "#252525")
+		 (180 . "#252525")
+		 (200 . "#252525")
+		 (220 . "#171717")
+		 (240 . "#171717")
+		 (260 . "#171717")
+		 (280 . "#0E0E0E")
+		 (300 . "#0E0E0E")
+		 (320 . "#0E0E0E")
+		 (340 . "#090909")
+		 (360 . "#090909"))))
+ '(vc-annotate-very-old-color "#3C3C3C"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
