6e9646f12e3d54cfb94d0bece2578535078ceff5
zovt <zovt@posteo.de>
2019-02-12 13:53:50 -0500

    Add some colors

--- emacs/init.el
+++ emacs/init.el
@@ -10,18 +10,36 @@
 
 (set-face-font 'default "Iosevka Term-17:weight=medium")
 (setq-default tab-width 8
-	      c-indent-offset 8
-	      c-default-style '((other . "k&r"))
-	      indent-tabs-mode t)
+              c-indent-offset 8
+              c-default-style '((other . "k&r"))
+              indent-tabs-mode t)
 (setq-default backup-by-copying t
-	      delete-old-versions t
-	      kept-new-versions 6
-	      kept-old-versions 2)
+              delete-old-versions t
+              kept-new-versions 6
+              kept-old-versions 2)
+
+(deftheme zovt "My colors")
+(let ((class '((class color) (min-colors 256)))
+      (default-face '(:foreground "black" :background "white")))
+  (custom-theme-set-faces
+   'zovt
+   `(default ((,class ,default-face)))
+   `(font-lock-doc-face ((,class ,default-face)))
+   `(font-lock-type-face ((,class ,default-face)))
+   `(font-lock-builtin-face ((,class ,default-face)))
+   `(font-lock-constant-face ((,class ,default-face)))
+   `(font-lock-string-face ((,class ,default-face)))
+   `(font-lock-keyword-face ((,class ,default-face)))))
+(provide-theme 'zovt)
+(provide 'zovt-theme)
+(enable-theme 'zovt)
+
 (setq-default backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 (add-hook 'after-init-hook 'server-start)
 
 (add-hook 'prog-mode-hook 'show-paren-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq indent-tabs-mode nil)))
 
 (setq-default confirm-kill-emacs 'yes-or-no-p)
 
