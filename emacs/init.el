345150f49bf797ff85f8f680334f518f881a7cd2
zovt <zovt@posteo.de>
2019-06-26 14:12:53 -0400

    Emacs: Speed up init

--- emacs/init.el
+++ emacs/init.el
@@ -1,42 +1,59 @@
-(setq-default custom-file "~/.emacs.d/custom.el")
+;; startup speed hacks
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
 
-;; package setup --
+(defun speedup-defer (time function)
+  "Runs function with idle timer"
+  (run-with-idle-timer time nil function))
+
+(setq-default gc-cons-threshold (* 50 1000 1000))
+
+;; package setup
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-;; ----------------
-
-;; install needed packages --
+
+;; install needed packages
 (defun install-if-needed (package-atom)
-  (interactive)
+  (interactive "Spackage: ")
   (if (not (package-installed-p package-atom))
       (package-install package-atom)
-    (require package-atom)))
+    (autoload package-atom (symbol-name package-atom) nil t)))
 
 (install-if-needed 'direx)
 (install-if-needed 'diminish)
-(install-if-needed 'magit)
 (install-if-needed 'ripgrep)
-;; --------------------------
-
-;; visual --
+(install-if-needed 'form-feed)
+(install-if-needed 'magit)
+
+;; visual
 (setq-default initial-scratch-message "")
 (setq-default inhibit-startup-message t)
 (setq-default visual-bell t)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
+(speedup-defer 0.1 (lambda () (tool-bar-mode 0)))
+(speedup-defer 0.1 (lambda () (menu-bar-mode 0)))
 (blink-cursor-mode 0)
 (scroll-bar-mode 0)
 (setq-default truncate-lines t)
 (setq-default mouse-autoselect-window -0.12)
 
-(set-face-font 'default "M+ 1m 14")
+(speedup-defer 0.1 (lambda () (set-face-font 'default "M+ 1m 14")))
 (set-fontset-font t 'unicode "EmojiOne" nil 'prepend)
 (setq-default backup-by-copying t
               delete-old-versions t
               kept-new-versions 6
               kept-old-versions 2)
+
+(setq-default scroll-preserve-screen-position t
+              scroll-conservatively 0
+              maximum-scroll-margin 0.5
+              scroll-margin 99999)
 
 (deftheme zovt "My colors")
 (let ((class '((class color) (min-colors 256)))
@@ -62,23 +79,29 @@
 (provide 'zovt-theme)
 (enable-theme 'zovt)
 
+(define-globalized-minor-mode global-form-feed-mode form-feed-mode
+  (lambda () (form-feed-mode 1)))
+(global-form-feed-mode 1)
+
+(speedup-defer 0.1 (lambda () (fringe-mode 20)))
+
 (diminish 'abbrev-mode)
-
-(fringe-mode 20)
-;; ---------
-
-(setq-default backup-directory-alist '(("." . "~/.emacs.d/backups")))
-
-;; hooks --
+(diminish 'auto-revert-mode)
+(diminish 'form-feed-mode)
+
+;; hooks
 (add-hook 'after-init-hook 'server-start)
-
 (add-hook 'prog-mode-hook 'show-paren-mode)
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'c++-mode-hook (lambda ()
                                 (setq c-noise-macro-names '("constexpr"))))
-;; --------
+
+;; other
+(setq-default backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq-default confirm-kill-emacs 'yes-or-no-p)
+(setq-default custom-file "~/.emacs.d/custom.el")
 
-;; dedicated windows --
+;; dedicated windows
 (defun mark-window-dedicated ()
   (interactive)
   (set-window-dedicated-p (selected-window) t)
@@ -91,9 +114,8 @@
 
 (global-set-key "\C-c\ d" 'mark-window-dedicated)
 (global-set-key "\C-c\ D" 'mark-window-not-dedicated)
-;; --------------------
-
-;; indentation ----
+
+;; indentation
 (defun newline-indent-match-previous ()
   (interactive)
   (let ((col (save-excursion
@@ -116,13 +138,13 @@
 (add-hook 'emacs-lisp-mode-hook (lambda () (setq indent-tabs-mode nil)))
 
 (electric-indent-mode 0)
-;; ----------------
-
-(setq-default confirm-kill-emacs 'yes-or-no-p)
-
-;; other keybinds --
+
+;; other keybinds
 (global-set-key "\C-t" 'hippie-expand)
 (global-set-key "\C-c\ c" 'compile)
-;; -----------------
+(global-set-key "\C-c\ l" (lambda () (interactive) (insert-char ?\^L)))
+
 
 (if (file-exists-p "~/.emacs.d/local.el") (load-file "~/.emacs.d/local.el"))
+
+(speedup-defer 0.1 (lambda () (setq gc-cons-threshold (* 2 1000 1000))))
