bbc751dcb040ae1edb0adc475547a7f2ab3847c0
zovt <zovt@posteo.de>
2019-07-03 13:26:15 -0400

    Emacs: Set up a bunch of packages
    Including IRC, Slack, and email

--- emacs/init.el
+++ emacs/init.el
@@ -17,6 +17,24 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+
+(defun read-file-to-string (file-name)
+  (with-temp-buffer
+    (insert-file-contents file-name)
+    (buffer-string)))
+
+(setq-default package-update-time-file "~/.emacs.d/package-update-time")
+(defun refresh-package-contents-daily ()
+  (if (not (file-exists-p package-update-time-file))
+      (progn (make-empty-file package-update-time-file)
+             (write-region "0" nil package-update-time-file)))
+  (let ((last-update (string-to-number (read-file-to-string package-update-time-file)))
+        (current-time (string-to-number (format-time-string "%s"))))
+    (if (> (- current-time last-update) 86400)
+        (progn (package-refresh-contents)
+               (write-region (number-to-string current-time)
+                             nil package-update-time-file)))))
+(refresh-package-contents-daily)
 (package-initialize)
 
 ;; install needed packages
@@ -30,6 +48,8 @@
 (install-if-needed 'form-feed)
 (install-if-needed 'magit)
 (install-if-needed 'mood-line)
+(install-if-needed 'esup)
+(install-if-needed 'slack)
 
 ;; visual
 (setq-default initial-scratch-message "")
@@ -49,11 +69,6 @@
               delete-old-versions t
               kept-new-versions 6
               kept-old-versions 2)
-
-(setq-default scroll-preserve-screen-position t
-              scroll-conservatively 0
-              maximum-scroll-margin 0.5
-              scroll-margin 99999)
 
 (deftheme zovt "My colors")
 (let ((class '((class color) (min-colors 256)))
@@ -100,7 +115,15 @@
                      " "))))))
 
 ;; hooks
-(add-hook 'after-init-hook 'server-start)
+(setq-default server-lock-file "~/.emacs.d/server-lock")
+(defun start-server-if-not-running ()
+  (if (not (file-exists-p server-lock-file))
+      (progn (make-empty-file server-lock-file)
+             (server-start)
+             (add-hook 'kill-emacs-hook
+                       (lambda () (delete-file server-lock-file))))))
+
+(add-hook 'after-init-hook 'start-server-if-not-running)
 (add-hook 'prog-mode-hook 'show-paren-mode)
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'c++-mode-hook (lambda ()
@@ -111,6 +134,8 @@
 (setq-default backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq-default confirm-kill-emacs 'yes-or-no-p)
 (setq-default custom-file "~/.emacs.d/custom.el")
+
+(setq-default epa-pinentry-mode 'loopback)
 
 ;; dedicated windows
 (defun mark-window-dedicated ()
@@ -164,7 +189,136 @@
 (global-set-key "\C-c\ l" (lambda () (interactive) (insert-char ?\^L)))
 (global-set-key "\C-c\ g" 'magit-status)
 
+;; utilities for configuring non-text-editor packages
+(require 'cl)
+(defun add-advice-once (trigger where function)
+  (lexical-let
+   ((lexical-trigger trigger)
+    (lexical-function function))
+   (advice-add trigger :after (lambda (&rest _rest) (advice-remove lexical-trigger lexical-function)))
+   (advice-add trigger where function))
+  nil)
 
+(defmacro lazy-configure (trigger-function &rest forms)
+  `(add-advice-once ,trigger-function :before (lambda (&rest _rest) ,@forms)))
+
+(defun send-process (process-name string)
+  (interactive "sprocess name: \nsinput: ")
+  (process-send-string process-name (concat string "\n")))
+
+;; erc
+(defun erc-autoconnect ()
+  (interactive)
+  (erc :server "irc.freenode.net")
+  (erc :server "irc.rizon.net"))
+
+(lazy-configure
+ 'erc
+ (add-to-list 'erc-modules 'notifications)
+ (erc-services-mode 1)
+ (erc-update-modules)
+ (setq-default erc-nick "zovt"
+               erc-nick-uniquifier "_"
+               erc-away-nickname "zovt [away]"
+               erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#gentoo")
+                                             ("rizon.net" "#rice")
+                                             ("oftc.net" "#llvm"))
+               erc-autojoin-timing 'ident
+               erc-prompt-for-nickserv-password nil
+               erc-kill-buffer-on-part t
+               erc-kill-queries-on-quit t
+               erc-kill-server-buffer-on-quit t
+               erc-autoaway-idle-seconds 600))
+
+;; slack
+(lazy-configure
+ 'slack-start
+ (setq-default slack-prefer-current-team t
+               slack-buffer-emojify t)
+ (slack-register-team
+  :name "FriendGroup"
+  :default t
+  :client-id "110c5ae1-1562126312.440"
+  :token "xoxs-112142280547-159670934352-676202934097-ed94b78db642c13de0d3b374799b8b292946bb220d09a917c43f534b8cbd5e0e"
+  :subscribed-channels '(programming mtg)
+  :full-and-display-names t))
+
+;; mu4e
+(lazy-configure
+ 'mu4e
+ (setq-default mu4e-maildir "~/.mail"
+               mu4e-sent-folder "/Posteo/Sent"
+               mu4e-drafts-folder "/Posteo/Drafts"
+               user-mail-address "nickippoliti@posteo.net"
+               user-full-name "Nick Ippoliti"
+               message-send-mail-function 'smtpmail-send-it
+               smtpmail-smtp-user "nickippoliti@posteo.net"
+               smtpmail-smtp-server "posteo.de"
+               smtpmail-smtp-service 587)
+ (defvar my/mu4e-account-alist
+   '(("Posteo"
+      (mu4e-sent-folder "/Posteo/Sent")
+      (user-mail-address "nickippoliti@posteo.net")
+      (user-full-name "Nick Ippoliti")
+      (smtpmail-smtp-user "nickippoliti@posteo.net")
+      (smtpmail-smtp-server "posteo.de")
+      (smtpmail-smtp-service 587))
+     ("School"
+      (mu4e-sent-folder "/School/[Gmail].Sent Mail")
+      (user-mail-address "ippoliti.n@husky.neu.edu")
+      (user-full-name "Nick Ippoliti")
+      (smtpmail-smtp-user "ippoliti.n@husky.neu.edu")
+      (smtpmail-smtp-server "smtp.gmail.com")
+      (smtpmail-smtp-service 587))
+      ))
+
+ (setq-default offlineimap-lock-file "~/.emacs.d/offlineimap-lock")
+ (defun start-offlineimap-if-needed (&rest _rest)
+   (unless (file-exists-p offlineimap-lock-file)
+     (make-empty-file offlineimap-lock-file)
+     (unless (get-buffer "*<offlineimap process>*")
+       (make-process :name "offlineimap" :buffer "*<offlineimap process>*"
+                     :command '("offlineimap")))
+     (add-hook 'kill-emacs-hook
+                       (lambda ()
+                         (delete-file offlineimap-lock-file)
+                         (if (get-process "offlineimap")
+                             (delete-process "offlineimap"))))))
+ (advice-add 'mu4e :before 'start-offlineimap-if-needed)
+ (start-offlineimap-if-needed)
+
+ (defun offline-imap-signin (account-name)
+   (interactive "saccount name: ")
+   (let ((account-file (concat "~/.emacs.d/mail/" account-name ".gpg")))
+     (send-process "offlineimap" (read-file-to-string account-file))))
+
+ (defun offline-imap-refresh ()
+   (interactive)
+   (signal-process "offlineimap" 'SIGUSR1))
+
+ (defun my/mu4e-set-account ()
+   "Set the account for composing a message.
+   This function is taken from:
+     https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
+   (let* ((account
+           (if mu4e-compose-parent-message
+               (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                 (string-match "/\\(.*?\\)/" maildir)
+                 (match-string 1 maildir))
+             (completing-read (format "Compose with account: (%s) "
+                                      (mapconcat #'(lambda (var) (car var))
+                                                 my/mu4e-account-alist "/"))
+                              (mapcar #'(lambda (var) (car var)) my/mu4e-account-alist)
+                              nil t nil nil (caar my/mu4e-account-alist))))
+          (account-vars (cdr (assoc account my/mu4e-account-alist))))
+     (if account-vars
+         (mapc #'(lambda (var)
+                   (set (car var) (cadr var)))
+               account-vars)
+       (error "No email account found"))))
+ (add-hook 'mu4e-compose-pre-hook 'my/mu4e-set-account))
+
+
 (if (file-exists-p "~/.emacs.d/local.el") (load-file "~/.emacs.d/local.el"))
 
 (speedup-defer 0.1 (lambda () (setq gc-cons-threshold (* 2 1000 1000))))
